0e2a25fc57333b5f324445d1db1dae639742e101
Rob Court <robbie.court@gmail.com>
2017-11-21 20:38:28 +0000

    direct links

--- process.sh
+++ process.sh
@@ -13,7 +13,7 @@ find . -name '*.gz' -exec pigz -dvf '{}' \;
 echo ''
 echo '** Side loading from vfb.owl: add_annonymous types **'
 
-export PYTHONPATH=/usr/local/lib/python.zip:/usr/local/lib/python:/usr/local/lib/python/lib-dynload:/usr/local/lib/python/site-packages:${WORKSPACE}/VFB_owl/src/code/mod/:${WORKSPACE}/VFB_owl/src/code/owl2neo/:${WORKSPACE}/VFB_owl/src/code/db_maintenance/:${WORKSPACE}/VFB_owl/src/code/entity_checks/:${WORKSPACE}/VFB_owl/src/code/export/:${WORKSPACE}/VFB_owl/src/code/owl_gen/:${WORKSPACE}/VFB_owl/src/code/unit_tests/
+export PYTHONPATH=/usr/local/lib/python27.zip:/usr/local/lib/python2.7:/usr/local/lib/python2.7/lib-dynload:/usr/local/lib/python2.7/site-packages:${WORKSPACE}/VFB_owl/src/code/mod/:${WORKSPACE}/VFB_owl/src/code/owl2neo/:${WORKSPACE}/VFB_owl/src/code/db_maintenance/:${WORKSPACE}/VFB_owl/src/code/entity_checks/:${WORKSPACE}/VFB_owl/src/code/export/:${WORKSPACE}/VFB_owl/src/code/owl_gen/:${WORKSPACE}/VFB_owl/src/code/unit_tests/
 
 sleep 10
 
