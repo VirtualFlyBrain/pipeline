f0c6b0edaed3e3f60ce828541319ad21c8e7c70b
Rob Court <robbie.court@gmail.com>
2017-11-22 14:41:49 +0000

    setting passphrase

--- process.sh
+++ process.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd "${WORKSPACE}"
-gpg --generate-key --batch
+gpg --generate-key --passwd default99 --batch
 echo '** Git checkout VFB_neo4j **'
 git clone --quiet https://github.com/VirtualFlyBrain/VFB_neo4j.git
 echo '** Git checkout hdietze/Brain **'
 git clone --quiet https://github.com/hdietze/Brain.git
 cd Brain
-mvn -DskipTests=true -Dmaven.javadoc.skip=true -Dsource.skip=true install
+mvn -Dgpg.passphrase=default99 -DskipTests=true -Dmaven.javadoc.skip=true -Dsource.skip=true install
 cd ${WORKSPACE}
 echo '** Git checkout VFB_owl **'
 git clone --quiet https://github.com/VirtualFlyBrain/VFB_owl.git
