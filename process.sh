0358179ad4e435d4aab54a53b9ab70d64d6b64ec
Rob Court <robbie.court@gmail.com>
2017-11-27 13:47:59 +0000

    listing key

--- process.sh
+++ process.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "${WORKSPACE}"
-gpg --full-gen-key --batch
-gpg --list-keys
+gpg --gen-key --batch
+gpg --list-secret-keys --keyid-format LONG
 echo '** Git checkout VFB_neo4j **'
 git clone --quiet https://github.com/VirtualFlyBrain/VFB_neo4j.git
 echo '** Git checkout hdietze/Brain **'
