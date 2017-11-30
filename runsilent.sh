f6888aaeda341424086f79680efeb7e5a630a2d2
Rob Court <robbie.court@gmail.com>
2017-11-30 12:16:26 +0000

    shell to report only errors

---
+++ runsilent.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Abort on Error
+set -e
+
+export JOBCMD=$1
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+   echo Tailing the last 500 lines of output:
+   tail -500 $BUILD_OUTPUT  
+}
+error_handler() {
+  echo ERROR: An error was encountered with the job.
+  dump_output
+  exit 1
+}
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+
+bash -c "while true; do echo \$(date)->$JOBCMD...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
+$JOBCMD >> $BUILD_OUTPUT 2>&1
+
+# The build finished without returning an error so dump a tail of the output
+dump_output
+
+# nicely terminate the ping output loop
+kill $PING_LOOP_PID
