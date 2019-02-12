4d4aa98280500256b12b9b085538d5c9d5f5c9bc
David Osumi-Sutherland <dosumis@gmail.com>
2019-02-12 17:56:18 +0000

    Update process.sh
    Switch script order in order to fix a bug with pub merging. Fixes https://github.com/VirtualFlyBrain/VFB_neo4j/issues/118

--- process.sh
+++ process.sh
@@ -7,19 +7,6 @@ cd ${WORKSPACE}/VFB_neo4j
 git pull origin master
 cd ..
 
-echo -e "travis_fold:start:add_refs_for_anat"
-echo '** Side loading from vfb owl: add refs **'
-if [ "${RUN_add_refs_for_anat}" != false ]
-then
-  sed -i -e "s/chunk_length=2000/chunk_length=${CHUNK_SIZE}/g" ${WORKSPACE}/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py
-  export BUILD_OUTPUT=${WORKSPACE}/add_refs_for_anat.out
-  ${WORKSPACE}/runsilent.sh "python3 ${WORKSPACE}/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py ${PDBSERVER} ${PDBuser} ${PDBpassword}"
-  cp $BUILD_OUTPUT /logs/
-  egrep 'Exception|Error|error|exception|warning' $BUILD_OUTPUT
-else
-  echo SKIPPED
-fi
-echo -e "travis_fold:end:add_refs_for_anat"
 
 echo ''
 echo -e "travis_fold:start:KB2Prod"
@@ -34,6 +21,20 @@ else
   echo SKIPPED
 fi
 echo -e "travis_fold:end:KB2Prod"
+
+echo -e "travis_fold:start:add_refs_for_anat"
+echo '** Expanding refs on anatomy terms **'
+if [ "${RUN_add_refs_for_anat}" != false ]
+then
+  sed -i -e "s/chunk_length=2000/chunk_length=${CHUNK_SIZE}/g" ${WORKSPACE}/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py
+  export BUILD_OUTPUT=${WORKSPACE}/add_refs_for_anat.out
+  ${WORKSPACE}/runsilent.sh "python3 ${WORKSPACE}/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/flybase2neo/add_refs_for_anat.py ${PDBSERVER} ${PDBuser} ${PDBpassword}"
+  cp $BUILD_OUTPUT /logs/
+  egrep 'Exception|Error|error|exception|warning' $BUILD_OUTPUT
+else
+  echo SKIPPED
+fi
+echo -e "travis_fold:end:add_refs_for_anat"
 
 echo ''
 echo -e "travis_fold:start:import_pub_data"
